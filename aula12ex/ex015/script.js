9ed88ff290c5d6fc66c68d5c28a61911441a8bd3
Thomas Benson <107052351+okokthomas@users.noreply.github.com>
2023-03-01 12:51:38 -0300

    finalizando o JS do verificador(falta o CSS e resolver o erro que deu)

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = window.document.querySelector('div#res')
+    if(fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementById('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement(img)
+        img.setAttribute('id', 'foto')
+        if(fsex[0].checked) {
+            genero = 'Homem'
+            if(idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'bebehomem.jpg')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'homemjovem.jpg')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'homemadulto.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'homemidoso.jpg')
+            }
+        } else if(fsex[1].checked) {
+            genero = 'Mulher'
+            if(idade >=0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'bebemulher.jpg')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'mulherjovem.jpg')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'mulheradulta.jpg')
+            } else {
+                //idoso
+                img.setAttribute('src', 'mulheridosa.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
