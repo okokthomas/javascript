c768d9480a6dc9b272928334d34ba093cdfe76e3
Thomas Benson <107052351+okokthomas@users.noreply.github.com>
2023-04-24 11:47:00 -0300

    vetor e arrays

---
+++ aula15/oi.js
@@ -0,0 +1,14 @@
+let num = [5,8,4,3,9,7,2,8]
+num[3] = 6
+
+num.sort()
+
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(num)
+let pos = num.indexOf(8)
+if(pos == -1) {
+    console.log(`O valor não foi encontrado`)
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
